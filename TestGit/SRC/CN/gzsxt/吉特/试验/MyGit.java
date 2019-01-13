4d60572e9f0d264a956364227c287eea153ee7ff
vacaee <46441619+vacaee@users.noreply.github.com>
2019-01-13 10:22:52 +0800

    第二次在中央服务器编辑MyGit类，修改了main方法。目的：测试冲突

--- TestGit/SRC/CN/gzsxt/吉特/试验/MyGit.java
+++ TestGit/SRC/CN/gzsxt/吉特/试验/MyGit.java
@@ -3,6 +3,7 @@ package cn.gzsxt.git.test;
 public class MyGit {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int a = 2;
 		System.out.println("hello,git");
 	}
 	public void testGit(){
